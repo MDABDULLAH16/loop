d550c9db9e7ed041474968afff75f3d04b7d15e8
MD ABDULLAH <mdabdullah161036@gmail.com>
2025-07-30 18:48:30 +0600

    total sum with while

---
+++ while.js
@@ -0,0 +1,19 @@
+// let number = 0;
+// //if the condition match then the loop will be continue;
+// while (number < 10) {
+//     number++;
+//     console.log(number);
+    
+// }
+
+let num = 0;
+let sum = 0;
+while (num < 5) {
+    sum = sum + num;
+    num++ 
+    // console.log(sum);
+    
+}
+console.log(sum);
+
+
