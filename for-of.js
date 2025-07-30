21608826a9e461bb7c79c5909c95118d3a520948
MD ABDULLAH <mdabdullah161036@gmail.com>
2025-07-30 14:51:26 +0600

    make word in sentence with array

--- for-of.js
+++ for-of.js
@@ -1,6 +1,22 @@
 const numbers = [23, 432, 123, 5, 34, 245, 2, 31, 3, 532]
 
 for (const number of numbers) {
-    console.log(number);
+    // console.log(number);
+    
+}
+
+// log the word 
+const names = 'amar sunar bangla re'
+const words = names.split(' ')
+console.log(words);
+
+for (const char of words) {
+    // console.log(nam);    
+    console.log(char);
+    
+    
+    
+    
+    
     
 }
