5baf1003774d64d095a7ecb6e3c12fe1955ce3c8
MD ABDULLAH <mdabdullah161036@gmail.com>
2025-07-30 18:58:21 +0600

    odd and even number with for-loop

---
+++ for-loop.js
@@ -0,0 +1,12 @@
+for (let i = 1; i <= 10; i++){
+    // console.log(i);
+    // the even number only
+    if (i % 2 === 0) {
+       console.log(i)
+    }
+    //odd number
+    if (i % 2 === 1) {
+       console.log(i)
+    }
+    
+}
